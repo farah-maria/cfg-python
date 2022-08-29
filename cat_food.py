6c6e0dc54548e47e06d63eb20d11653979c6e83f
Farah M <farah.maria.rahman@gmail.com>
2022-08-29 23:27:33 +0000

    add cat_food program

---
+++ cat_food.py
@@ -0,0 +1,10 @@
+
+
+cats = 5
+cans_per_day_per_cat = 2
+days = 7
+
+total_cans = cats * cans_per_day_per_cat * days
+
+print(f"{cats} cats eat {str(total_cans)} cans in {days} days.")
+
--- run.py
+++ run.py
@@ -1,3 +1,36 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("hello world") # a function is a reusable piece of code that completes a specific task. They are usually a word followed by round brackets.
+print(5 - 6)
+print(8 * 9) # multiplication
+print(6 / 2)
+# print(5 / 0) - ZeroDivisionError
+print(5.0 / 2) # float division
+print (5 % 2) # modulo (remainder)
+print( 2 * (10 + 3))
+print(2 ** 4) # exponent (x to the power of y)
+
+print('this is a string')
+print("this is also a string")
+print("Cat")
+print("Cat" + "videos") # concatenation
+print("Cat" * 3)
+#print("Cat" + 3) TypeError: can only concatenate str (not "int") to str
+print("Cat".upper()) # method - a repeatable piece of code that completes a task for a specific data type
+print("Cat".lower()) # method
+print("the lord of the rings".title())
+print("Cat" + str(3))
+
+# A variable can be reused. This program calculates the cost of
+# 12 oranges
+
+oranges = 12
+cost_per_orange = 0.5
+
+total_cost = oranges * cost_per_orange
+
+print(str(oranges) + "oranges")
+print("costs " + str(total_cost))
+
+
+
+
