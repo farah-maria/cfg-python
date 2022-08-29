8ff48485a13c88c8d9efe5c460665858fe1a1811
Farah M <farah.maria.rahman@gmail.com>
2022-08-29 23:51:20 +0000

    add homework

---
+++ last.py
@@ -0,0 +1,10 @@
+days = 31
+hours = "24"
+total_hours = days * hours
+
+output = "There are {} in {} days".format(total_hours, days)
+print(output)
+
+# = 2424242424242424242424242424242424242
+# 4242424242424242424242424 in 31 days
+# ( thirty-one 24s!!!)
