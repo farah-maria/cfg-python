a5ac74e4b2fe7a70ca8da67193ad03eb9e54393d
Farah M <farah.maria.rahman@gmail.com>
2022-08-29 23:41:31 +0000

    add alternative string method that uses 'format'

--- cat_food.py
+++ cat_food.py
@@ -6,5 +6,19 @@ days = 7
 
 total_cans = cats * cans_per_day_per_cat * days
 
-print(f"{cats} cats eat {str(total_cans)} cans in {days} days.")
+#print(f"{cats} cats eat {str(total_cans)} cans in {days} days.")
+
+
+output = "{} cats eat {} cans in {} days".format(cats, cans_per_day_per_cat, days)
+
+print(output)
+
+
+"""user_name = "sarah_1987"
+age = 23
+
+output = '{} is {} years old'.format(user_name, age)
+print(output)"""
+
+
 
